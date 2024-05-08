70e308e1c24386d14ebb10f33afd84ec7fae151e
zvacura_nepgroup <zvacura@nepgroup.com>
2024-05-08 18:37:18 -0400

    redesign website

---
+++ uno.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'unocss';
+import presetWebFonts from '@unocss/preset-web-fonts';
+import presetUno from '@unocss/preset-uno';
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        emoji: "Noto Color Emoji",
+      },
+    }),
+  ],
+});
